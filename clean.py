a28128f455b2e1893a13a504c9b9422bae5d66f0
cyang-05 <chiashiiyang@gmail.com>
2026-03-29 20:23:02 -0400

    new csv

--- clean.py
+++ clean.py
@@ -3,3 +3,9 @@ import pandas as pd
 df = pd.read_csv('copy.csv')
 df.dropna()
 df.to_csv('cleaned.csv')
+
+df = df[df['type'] != 'TV Show']
+df.to_csv('updated.csv')
+
+df = df.drop('description', axis=1)
+df.to_csv('final updated.csv')
