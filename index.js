ebaf9334835446574949d56e3a4171e34b56b5a2
kiaras-create <ksunilsingh@smith.edu>
2026-03-31 17:59:07 -0400

    converting genres to values

--- index.js
+++ index.js
@@ -1,57 +1,81 @@
+
 d3.csv("cleaned.csv")
-    .then(function(data){
+    .then(function (data) {
         const links = getCol(data);
-        const matrixData = links.map(d =>  ({
+        const matrixData = links.map(d => ({
             row: d.source,
             col: d.target,
             value: d.value
-}));
-        getCol(data);
-        //update(data);
-        
-    });
+        }));
 
-function update(data){
+
+        const wordToId = new Map();
+
+        let index = 0;
+
+        links.forEach(d => {
+            if (!wordToId.has(d.source)) {
+                wordToId.set(d.source, index++);
+            }
+            if (!wordToId.has(d.target)) {
+                wordToId.set(d.target, index++)
+            }
+        })
+
+        const numericalGenre = links.map(d => ({
+            source: wordToId.get(d.source),
+            target: wordToId.get(d.target),
+            value: +d.value
+        }));
+        //update(data);
+        console.log(numericalGenre)
+        });
+
+    
+
+function update(data) {
     console.log(data);
 }
 
-function getCol(data){
+function getCol(data) {
     const links = [];
 
     data.forEach(d => {
 
-        if (!d.listed_in) 
+        if (!d.listed_in)
             return
         const genres = d.listed_in.split(", ");
- 
-        for (let i = 0; i < genres.length; i++){
-            for (let j = i + 1; j< genres.length; j++) {
+
+        for (let i = 0; i < genres.length; i++) {
+            for (let j = i + 1; j < genres.length; j++) {
                 links.push({
-            //First part of connection
-            source: genres[i],
-            //Second part of connection
-            target: genres[j],
-            value: 1
-        });
+                    //First part of connection
+                    source: genres[i],
+                    //Second part of connection
+                    target: genres[j],
+                    value: 1
+                });
                 links.push({
-            //First part of connection
-                source: genres[j],
-                //Second part of connection
-                target: genres[i],
-                value: 1
-        });
+                    //First part of connection
+                    source: genres[j],
+                    //Second part of connection
+                    target: genres[i],
+                    value: 1
+                });
+            }
         }
-    }
-        
+
+
     });
-    
-    const myData = data.map(d => ({
-        //Make each genres into array
-        Genre: d.listed_in ? d.listed_in.split(", "): []
-    }));
-    console.log(links)
     return links;
-}
+    };
+
+
+
+
+
+
+
 
 
 
